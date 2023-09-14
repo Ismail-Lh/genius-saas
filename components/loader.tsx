d9c23303949aac820b59a6103c91e01b3788c8ea
Ismail-Lh <ismailsaad03@gmail.com>
2023-09-14 16:56:17 +0100

    feat: create the conversation AI functionality part-3

--- components/loader.tsx
+++ components/loader.tsx
@@ -1,5 +1,14 @@
+import Image from 'next/image';
+
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div className="h-full flex flex-col gap-y-4 items-center justify-center">
+      <div className="w-10 h-10 relative animate-spin">
+        <Image fill alt="logo" src="/assets/logo.png" />
+      </div>
+      <p className="text-sm text-muted-foreground">Genius is Thinking...</p>
+    </div>
+  );
 };
 
 export default Loader;
