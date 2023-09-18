b03d2d905759facbd3f0142fb36de3fe40d8a002
Ismail-Lh <ismailsaad03@gmail.com>
2023-09-18 19:00:39 +0100

    feat: create the Image generation page UI - part-1

---
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -0,0 +1,151 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios from 'axios';
+import { ImageIcon, MessageSquare } from 'lucide-react';
+import { ChatCompletionRequestMessage } from 'openai';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+import { cn } from '@/lib/utils';
+import BotAvatar from '@/components/bot-avatar';
+import Empty from '@/components/empty';
+import Heading from '@/components/heading';
+import Loader from '@/components/loader';
+import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import UserAvatar from '@/components/user-avatar';
+import { amountOptions, formSchema } from './constants';
+
+function ImagePage() {
+  const router = useRouter();
+  const [images, setImages] = useState<string[]>([]);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: '',
+      amount: '1',
+      resolution: '512x512',
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setImages([]);
+      const response = await axios.post('/api/image', values);
+
+      const imagesUrl = response.data.map(
+        (image: { url: string }) => image.url
+      );
+
+      setImages(imagesUrl);
+
+      form.reset();
+    } catch (error: any) {
+      // TODO: open a PRO model
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="Image Generation"
+        description="Turn your prompt into an image"
+        icon={ImageIcon}
+        iconColor="text-pink-700"
+        bgColor="bg-pink-700/10"
+      />
+
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="Turn prompt text into images..."
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="amount"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <Select
+                      disabled={isLoading}
+                      onValueChange={field.onChange}
+                      value={field.value}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue defaultValue={field.value} />
+                        </SelectTrigger>
+                      </FormControl>
+
+                      <SelectContent>
+                        {amountOptions.map((option) => (
+                          <SelectItem key={option.value} value={option.value}>
+                            {' '}
+                            {option.label}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 lg:col-span-2 w-full"
+                disabled={isLoading}
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-20">
+              <Loader />
+            </div>
+          )}
+          {images.length === 0 && !isLoading && (
+            <Empty label="No images generated." />
+          )}
+          <div>Images will be rendered here</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ImagePage;
