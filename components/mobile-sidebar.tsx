a58ba2885b70cff6151e1f61dedec631a05828db
Ismail-Lh <ismailsaad03@gmail.com>
2023-10-20 16:16:52 +0100

    feat(UI): add the apiLimitCount number to the mobile sidebar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -7,7 +7,11 @@ import Sidebar from '@/components/sidebar';
 import { Button } from '@/components/ui/button';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 
-const MobileSidebar = () => {
+interface MobileSideBarProps {
+  apiLimitCount: number;
+}
+
+const MobileSidebar = ({ apiLimitCount }: MobileSideBarProps) => {
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
@@ -24,7 +28,7 @@ const MobileSidebar = () => {
         </Button>
       </SheetTrigger>
       <SheetContent side="left" className="p-0">
-        <Sidebar />
+        <Sidebar apiLimitCount={apiLimitCount} />
       </SheetContent>
     </Sheet>
   );
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
 import { UserButton } from '@clerk/nextjs';
 
+import { getApiLimitCount } from '@/lib/api-limit';
 import MobileSidebar from './mobile-sidebar';
 
-const Navbar = () => {
+const Navbar = async () => {
+  const apiLimitCount = await getApiLimitCount();
+
   return (
     <div className="flex items-center p-4">
-      <MobileSidebar />
+      <MobileSidebar apiLimitCount={apiLimitCount} />
 
       <div className="flex justify-end w-full">
         <UserButton afterSignOutUrl="/" />
